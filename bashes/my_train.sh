185138789748493ae05e3da4f413647fe721ef38
kravchenko01 <roma.kravchenco.2001@mail.ru>
2024-09-24 21:00:47 +0300

    fix

--- bashes/my_train.sh
+++ bashes/my_train.sh
@@ -1,3 +1,3 @@
 export output_dir="./res/rdgv3"
 export datasets_path="./datasets"
-python train_seg.py -cfg ./configs/segmentation.yaml -opts MODELS.OUT_ADV True TRAIN.BATCH_SIZE 32 DATASETS.EVL_BATCH 32 DATASETS.SOURCE_DATASET_PATH $datasets_path/potsdam OUTPUT_DIR $output_dir
+python train_seg.py -cfg ./configs/segmentation.yaml -opts MODELS.OUT_ADV True TRAIN.BATCH_SIZE 8 DATASETS.EVL_BATCH 8 DATASETS.SOURCE_DATASET_PATH $datasets_path/potsdam OUTPUT_DIR $output_dir
