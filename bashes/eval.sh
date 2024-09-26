50179227c6e30b6c3911f95752934e179fa0dfd9
kravchenko01 <roma.kravchenco.2001@mail.ru>
2024-09-26 20:46:13 +0300

    change target images size to 112x112

--- bashes/eval.sh
+++ bashes/eval.sh
@@ -1,5 +1,5 @@
 export model_path="./res/rdgv3/model/model.pt"
-export output_dir="./res"
+export output_dir="./res/rdgv3"
 export evl_data_path="./datasets/our"
 export part="all"
 python evaluate.py --model_path $model_path -cfg ./configs/segmentation.yaml -opts DATASETS.EVL_BATCH 32 OUTPUT_DIR $output_dir DATASETS.EVL_DATASET_PATH $evl_data_path DATASETS.EVL_PART $part
