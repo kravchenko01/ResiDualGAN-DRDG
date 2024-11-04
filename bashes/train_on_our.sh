82656c9554db63520162bbc709bc5e2e2c6f56f2
kravchenko01 <roma.kravchenco.2001@mail.ru>
2024-11-05 01:08:17 +0300

    set batch_size=1 when train_on_our

--- bashes/train_on_our.sh
+++ bashes/train_on_our.sh
@@ -1,3 +1,3 @@
 export output_dir="./res/train_on_our"
 export datasets_path="./datasets"
-python train_seg.py -cfg ./configs/segmentation.yaml -opts MODELS.OUT_ADV True TRAIN.BATCH_SIZE 2 DATASETS.EVL_BATCH 8 DATASETS.SOURCE_DATASET_PATH $datasets_path/our DATASETS.SOURCE_PART train DATASETS.EVL_PART test OUTPUT_DIR $output_dir
+python train_seg.py -cfg ./configs/segmentation.yaml -opts MODELS.OUT_ADV True TRAIN.BATCH_SIZE 2 DATASETS.EVL_BATCH 1 DATASETS.SOURCE_DATASET_PATH $datasets_path/our DATASETS.SOURCE_PART train DATASETS.EVL_PART test OUTPUT_DIR $output_dir
