2298bbb8457bfcf5ce9a66293fec6ac852ac82c5
kravchenko01 <roma.kravchenco.2001@mail.ru>
2025-04-04 14:22:41 +0300

    fix path

--- bashes/potsdam2304_mpia/train_from_scratch.sh
+++ bashes/potsdam2304_mpia/train_from_scratch.sh
@@ -1,3 +1,3 @@
 export output_dir="./res/rdgv3_scratch"
 export datasets_path="./datasets"
-python train_seg.py -cfg ./configs/potsdam2304_mpia/segmentation.yaml -opts DATASETS.SOURCE_DATASET_PATH $datasets_path/potsdam_2304 OUTPUT_DIR $output_dir
+python train_seg.py -cfg ./bashes/potsdam2304_mpia/segmentation.yaml -opts DATASETS.SOURCE_DATASET_PATH $datasets_path/potsdam_2304 OUTPUT_DIR $output_dir
