db16c5f218f3bdea742bd52bc7ffde6461dbd510
kravchenko01 <roma.kravchenco.2001@mail.ru>
2025-04-04 03:36:47 +0300

    fix size 672 to 2304

--- bashes/potsdam672_mpia/train_from_transformed.sh
+++
@@ -1,3 +0,0 @@
-export output_dir="./res/rdgv3"
-export datasets_path="./datasets"
-python train_seg.py -cfg ./bashes/potsdam672_mpia/segmentation.yaml -opts DATASETS.SOURCE_DATASET_PATH $output_dir/data OUTPUT_DIR $output_dir
--- bashes/potsdam672_mpia/train_rdg.sh
+++
@@ -1,3 +0,0 @@
-export output_dir="./res/rdgv3"
-export datasets_path="./datasets"
-python train_residualgan.py -cfg ./bashes/potsdam672_mpia/residualgan.yaml -opts OUTPUT_DIR $output_dir DATASETS.SOURCE_PATH $datasets_path/potsdam_672 DATASETS.TARGET_PATH $datasets_path/our_mpia
