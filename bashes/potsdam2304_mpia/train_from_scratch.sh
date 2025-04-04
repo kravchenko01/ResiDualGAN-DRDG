f159ce27f01e198ccc8d0c630fe6a73629b57ca8
kravchenko01 <roma.kravchenco.2001@mail.ru>
2025-04-04 14:20:15 +0300

    Add script for train from scratch

---
+++ bashes/potsdam2304_mpia/train_from_scratch.sh
@@ -0,0 +1,3 @@
+export output_dir="./res/rdgv3_scratch"
+export datasets_path="./datasets"
+python train_seg.py -cfg ./configs/potsdam2304_mpia/segmentation.yaml -opts DATASETS.SOURCE_DATASET_PATH $datasets_path/potsdam_2304 OUTPUT_DIR $output_dir
