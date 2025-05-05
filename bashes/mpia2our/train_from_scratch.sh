a59fada280def3ca7df8bfa3b1a17576cf4000b9
kravchenko01 <roma.kravchenco.2001@mail.ru>
2025-05-05 18:32:34 +0300

    fix

--- bashes/mpia2our/train_from_scratch.sh
+++ bashes/mpia2our/train_from_scratch.sh
@@ -1,3 +1,3 @@
-export output_dir="./res/mpi2our"
+export output_dir="./res/mpia2our"
 export datasets_path="./datasets"
-python train_seg.py -cfg ./bashes/mpi2our/segmentation.yaml -opts DATASETS.SOURCE_DATASET_PATH $datasets_path/our_mpi OUTPUT_DIR $output_dir
+python train_seg.py -cfg ./bashes/mpia2our/segmentation.yaml -opts DATASETS.SOURCE_DATASET_PATH $datasets_path/our_mpia OUTPUT_DIR $output_dir
