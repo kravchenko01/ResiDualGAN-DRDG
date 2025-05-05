cb6009b1a9ef439cfe7aeef9ad208cf87f537857
kravchenko01 <roma.kravchenco.2001@mail.ru>
2025-05-05 18:11:45 +0300

    fix

--- bashes/our2mpia/eval_scratch.sh
+++ bashes/our2mpia/eval_scratch.sh
@@ -1,5 +1,5 @@
-export model_path="./res/our2mpi/model/model.pt"
-export output_dir="./res/our2mpi"
+export model_path="./res/our2mpia/model/model.pt"
+export output_dir="./res/our2mpia"
 export evl_data_path="./datasets/our_mpia"
 export part="all"
-python evaluate.py --model_path $model_path -cfg ./bashes/our2mpi/segmentation.yaml -opts DATASETS.EVL_BATCH 32 OUTPUT_DIR $output_dir DATASETS.EVL_DATASET_PATH $evl_data_path DATASETS.EVL_PART $part
+python evaluate.py --model_path $model_path -cfg ./bashes/our2mpia/segmentation.yaml -opts DATASETS.EVL_BATCH 32 OUTPUT_DIR $output_dir DATASETS.EVL_DATASET_PATH $evl_data_path DATASETS.EVL_PART $part
--- bashes/our2mpia/train_from_scratch.sh
+++ bashes/our2mpia/train_from_scratch.sh
@@ -1,3 +1,3 @@
-export output_dir="./res/our2mpi"
+export output_dir="./res/our2mpia"
 export datasets_path="./datasets"
-python train_seg.py -cfg ./bashes/our2mpi/segmentation.yaml -opts DATASETS.SOURCE_DATASET_PATH $datasets_path/our OUTPUT_DIR $output_dir
+python train_seg.py -cfg ./bashes/our2mpia/segmentation.yaml -opts DATASETS.SOURCE_DATASET_PATH $datasets_path/our OUTPUT_DIR $output_dir
